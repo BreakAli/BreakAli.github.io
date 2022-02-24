438e2c55fe2d12fa0e0ac497aef054a19f6b9a52
BreakAli <adarawsha1@gmail.com>
2022-02-23 22:48:03 -0600

    1

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,15 +1,13 @@
 <?php
   /**
   * Requires the PHP Mail Form library
-  * The PHP Mail Form library is available only in the pro version of the template
   * The library should be uploaded to: lib/php-mail-form/php-mail-form.php
-  * For more info and help: https://templatemag.com/php-mail-form/
   */
 
   if( file_exists($php_mail_form_library = '../lib/php-mail-form/php-mail-form.php' )) {
     include( $php_mail_form_library );
   } else {
-    die( 'Unable to load the PHP Mail Form Library!');
+    die( 'Unable to load the PHP Mail From Library!');
   }
 
   $contactform = new PHP_Mail_Form;
